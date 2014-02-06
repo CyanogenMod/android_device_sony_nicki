6a40fee49c94f12410dc0f65a323fdd50cabfd5f
RonGokhale <cips173@gmail.com>
2014-02-06 13:17:43 +0530

    c1905: Add Initial SonyQualcomm8x27RIL
    for manual network search
    
    to use add ro.telephony.ril_class=SonyQualcomm8x27RIL
    
    Change-Id: I17ab8fc59d8944c387e22f0ce6d8ff5136bd58cf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,8 @@ TARGET_BOOTLOADER_BOARD_NAME := qcom
 
 TARGET_OTA_ASSERT_DEVICE := c1904,c1905,nicki
 
+BOARD_RIL_CLASS := ../../../device/sony/c1905/ril/
+
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
