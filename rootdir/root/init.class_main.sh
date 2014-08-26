05a479bdafe0e29d56b9ffe3cc3fe9a6e8f320d3
RonGokhale <cips173@gmail.com>
2014-08-26 16:14:11 +0530

    nicki: clean-up class_main script
     - use this script only for starting 2nd ril daemon
    
    Change-Id: I46e0f0f5afdd10cd06fea3ee83fd07284e3b3c98

--- rootdir/root/init.class_main.sh
+++ rootdir/root/init.class_main.sh
@@ -29,45 +29,7 @@
 #
 # start ril-daemon only for targets on which radio is present
 #
-baseband=`getprop ro.baseband`
-netmgr=`getprop ro.use_data_netmgrd`
-sgltecsfb=`getprop persist.radio.sglte_csfb`
-
-case "$baseband" in
-    "apq")
-    setprop ro.radio.noril yes
-    stop ril-daemon
-esac
-
-case "$baseband" in
-    "msm" | "csfb" | "svlte2a" | "mdm" | "sglte" | "sglte2" | "dsda2" | "unknown")
-    start qmuxd
-    case "$baseband" in
-        "svlte2a" | "csfb")
-          start qmiproxy
-        ;;
-        "sglte" | "sglte2" )
-          if [ "x$sgltecsfb" != "xtrue" ]; then
-              start qmiproxy
-          else
-              setprop persist.radio.voice.modem.index 0
-          fi
-        ;;
-        "dsda2")
-          setprop persist.radio.multisim.config dsda
-    esac
-
-    multisim=`getprop persist.radio.multisim.config`
-
-    if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-        start ril-daemon1
-    elif [ "$multisim" = "tsts" ]; then
-        start ril-daemon1
-        start ril-daemon2
-    fi
-
-    case "$netmgr" in
-        "true")
-        start netmgrd
-    esac
-esac
+multisim=`getprop persist.radio.multisim.config`
+if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
+    start ril-daemon1
+fi
