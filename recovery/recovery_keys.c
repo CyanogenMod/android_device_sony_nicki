da43ec780a81ee0c8ab97bbb3ce6cafc7594e843
RonGokhale <cips173@gmail.com>
2014-09-16 15:55:59 +0530

    nicki: Fix enter and back in swipe
    and cleanup
    
    Change-Id: I608b932bb61064f52177bccf7e6061dc5b14b3be

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -4,11 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    return 0;
-}
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
@@ -19,8 +14,10 @@ int device_handle_key(int key_code, int visible) {
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
+            case KEY_ENTER:
                 return SELECT_ITEM;
 
+            case KEY_BACK:
             case KEY_CAMERA_FOCUS:
                 if (!ui_root_menu) {
                     return GO_BACK;
