c0d24da36aa90de24814bce1cc21af4b8c72b8a4
Soham Lad <sohamlad7@gmail.com>
2015-02-24 04:19:22 -0800

    Create omni_nicki.mk

---
+++ omni_nicki.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 The OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit AOSP device config
+$(call inherit-product, device/sony/taoshan/full_nicki.mk)
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Overrides for Omni
+PRODUCT_DEVICE := nicki
+PRODUCT_NAME := omni_nicki
