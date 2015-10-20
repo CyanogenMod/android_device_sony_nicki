8e8b1a488d1d5ce47a7c832f4a33f5b339e2bdbe
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-10-20 07:43:13 +0200

    nicki: explicitly build recovery image target

--- custombootimg.mk
+++ custombootimg.mk
@@ -35,6 +35,7 @@ INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	$(recovery_ramdisk) \
 	$(recovery_kernel)
+	$(call build-recoveryimage-target, $@)
 	@echo ----- Making recovery image ------
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
 	@echo ----- Made recovery image -------- $@
