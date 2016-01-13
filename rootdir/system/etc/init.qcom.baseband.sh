65b1fc8ec1fcf78ff922a9f468acea81cc03e789
RonGokhale <cips173@gmail.com>
2016-01-13 11:33:57 +0530

    nicki: Stop using busybox for setting beseband version
     - re-use bootdevice symlink
     - search bb version only
    
    Change-Id: Id1b009627ed59e9a11232c6bc2e9bb956d12c458

--- rootdir/system/etc/init.qcom.baseband.sh
+++ rootdir/system/etc/init.qcom.baseband.sh
@@ -1,3 +1,3 @@
 #!/system/bin/sh
 
-setprop gsm.version.baseband `busybox strings /dev/block/platform/msm_sdcc.1/by-name/modem  | busybox grep "M8930B-" | head -1`
+setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem  | grep "M8930B-AAAATAZM" | head -1`
