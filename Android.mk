8d5b313e49ed7bf1507a7e954d8ddc414e316cc3
RonGokhale <cips173@gmail.com>
2016-05-14 12:15:20 +0530

    nicki: Use new API for audio calibration
    Change-Id: Ia660ff639659005d9199f4dfc19690bf269a2670

--- Android.mk
+++ Android.mk
@@ -37,12 +37,6 @@ $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
 
-      $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9310; \
-        ln -sf /data/misc/audio/wcd9310_anc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
-        ln -sf /data/misc/audio/mbhc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin)
-
       $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
         ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
         $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin)
