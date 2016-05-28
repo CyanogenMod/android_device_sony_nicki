253aad9d0849fcd34a8aaf08c83a48952f44dcac
Adrian DC <radian.dc@gmail.com>
2016-05-28 03:52:57 +0200

    nicki: Restructure the device tree into fragments
     * Explode all configurations and makefiles
        into feature-related files automatically imported
    
     * Based upon the fragmented structure introduced
        in android_device_nextbit_ether by Steve Kondik,
        and the previous implementation for Huashan
    
     * Additional relative paths usage and sources cleanup
    
    Change-Id: I952bc49056108eb912bc7b0b7366ba2122632611
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -1,20 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
+DEVICE_BOOTDIR := device/sony/nicki/boot
+DEVICE_LOGORLE := $(DEVICE_BOOTDIR)/logo.rle
+INITSONY := $(PRODUCT_OUT)/utilities/init_sony
+
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
-DEVICE_ROOTDIR := device/sony/nicki/rootdir/root
-INITSONY := $(PRODUCT_OUT)/utilities/init_sony
-
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(INITSONY) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Boot image: $@")
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
+		$(uncompressed_ramdisk) \
+		$(recovery_uncompressed_ramdisk) \
+		$(INSTALLED_RAMDISK_TARGET) \
+		$(INITSONY) \
+		$(PRODUCT_OUT)/utilities/toybox \
+		$(PRODUCT_OUT)/utilities/keycheck \
+		$(MKBOOTIMG) $(MINIGZIP) \
+		$(INTERNAL_BOOTIMAGE_FILES)
 
+	$(call pretty,"Boot image: $@")
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
 
-	$(hide) cp $(DEVICE_ROOTDIR)/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
+	$(hide) cp $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
 	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
@@ -30,8 +39,8 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-	$(recovery_ramdisk) \
-	$(recovery_kernel)
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
 	$(call build-recoveryimage-target, $@)
 	@echo ----- Making recovery image ------
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,8 +1,11 @@
+# Copyright 2013 The Android Open Source Project
+# Copyright 2015 The CyanogenMod Project
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := healthd_board_nicki.cpp
-LOCAL_MODULE := libhealthd.nicki
-LOCAL_CFLAGS := -Werror
+LOCAL_SRC_FILES := healthd_board_msm.cpp
+LOCAL_MODULE := libhealthd.msm
 LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
+LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)
