b89cedb1b61df01101187a25028ab7c91741bb98
RonGokhale <cips173@gmail.com>
2016-07-26 16:06:18 +0530

    nicki: Move wlan dat file to device tree
    Change-Id: I11362ea8a4ee1218bb285f6c7e5040acd3f33b0b

--- product/wifi.mk
+++ product/wifi.mk
@@ -1,5 +1,6 @@
 # WiFi WCNSS configurations
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
 # WPA supplicant configurations
