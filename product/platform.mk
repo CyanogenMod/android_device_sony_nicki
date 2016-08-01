9433b2ee07a9073eb8e7c35ba0623e5c9050d6ee
RonGokhale <cips173@gmail.com>
2016-08-01 18:40:01 +0530

    nicki: Move tad_static to vendor
    Change-Id: I56faf9b6e9b5b62a9fbc6f30f022ce82e95d33b3

--- product/platform.mk
+++ product/platform.mk
@@ -3,7 +3,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
-
-# Sony TrimArea package
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/sbin/tad_static:root/sbin/tad_static
