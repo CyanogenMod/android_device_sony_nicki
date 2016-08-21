4d215c2510cdf55bcf29afe6e642a5be6ce525af
Adrian DC <radian.dc@gmail.com>
2016-08-22 01:46:31 +0200

    nicki: settings: Disable recovery update
     * Sony devices do not require this feature to be listed,
        as the recovery is included in the ramdisk and
        will be updated automatically with the ROM
    
    Change-Id: I1b84f05069862ca131f635a95de8078a72860514

--- product/platform.mk
+++ product/platform.mk
@@ -3,3 +3,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
+
+# Updater properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.disable.recovery.updater=1
